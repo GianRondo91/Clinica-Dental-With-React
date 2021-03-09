0ffb63d3be0fd8f8f4e3bb72de0fd5182e9aee83
Giancarlo <giancarlorondo@outlook.com>
2021-03-09 20:14:37 +0100

    Estructuring project

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Header from '../../components/Header';
+
+class Home extends React.Component {
+    // constructor (props){
+    //     super(props)
+    // };
+
+    render(){
+        return (
+            <div className="homeView">
+
+            </div>
+        )
+    }
+}
+export default Home;

4d577c27663249d9201335ee10d6f375cb5cf886
Giancarlo <giancarlorondo@outlook.com>
2021-03-11 13:53:06 +0100

    testing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 class Header extends React.Component {
     // constructor (props){
@@ -9,22 +12,20 @@ class Header extends React.Component {
         return (
 
             <div id="header" className="cell-3">
-                <div className="img">
-                    Logo
-                    </div>
+                <div className="img"></div>
 
                 <div className="menu-list">
                     <ul className="menu-list-ul">
                         <li>Home</li>
                         <li>Promociones</li>
-                        <li>Sobre Nosotros</li>
+                        <li>Sobre Nosotros<FontAwesomeIcon icon={faCoffee} /></li>
                         <li>Contacto</li>
                     </ul>
                 </div>
 
                 <div className="buttons">
                     <div className="button1">Login</div>
-                    <div className="button2">Logout</div>
+                    <div className="button2">Register <FontAwesomeIcon icon={["fal", "coffee"]}/></div>
                 </div>
 
             </div>
